d167abc0e4f44b4932a99039b1d6979c296848d9
Russimar <russimar.s@gmail.com>
2017-01-10 11:35:37 -0200

    Ok

---
+++ appNfse/features/CAD/cad_empresa/services.js
@@ -0,0 +1,37 @@
+
+
+var __extends = (this && this.__extends) || function (d, b) {
+    for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
+    function __() { this.constructor = d; }
+    d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+};
+
+var App;
+(function (App) {
+    var Services;
+    (function (Services) {
+        "use strict";
+        var CrudCad_EmpresaService = (function (_super) {
+            __extends(CrudCad_EmpresaService, _super);
+
+            function CrudCad_EmpresaService($q, api) {
+                _super.apply(this, arguments);                
+            }
+
+            Object.defineProperty(CrudCad_EmpresaService.prototype, "baseEntity", {
+                /// @override
+                get: function () {
+                    return 'CAD_EMPRESA';
+                },
+                enumerable: true,
+                configurable: true
+            });            
+   
+            return CrudCad_EmpresaService;
+        })(Services.CrudBaseService);
+        Services.CrudCad_EmpresaService = CrudCad_EmpresaService;
+        App.modules.Services
+            .service('CrudCad_EmpresaService', CrudCad_EmpresaService);
+    })(Services = App.Services || (App.Services = {}));
+})(App || (App = {}));
+//# sourceMappingURL=services.js.map
