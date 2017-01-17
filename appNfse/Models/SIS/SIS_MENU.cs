d3ea161f10c56af3fd9f9816856564c00b8910bb
Russimar <russimar.s@gmail.com>
2017-01-17 20:00:54 -0200

    Sis_menu completo

--- appNfse/Models/SIS/SIS_MENU.cs
+++ appNfse/Models/SIS/SIS_MENU.cs
@@ -18,15 +18,19 @@
         [Required]
         public int GRUPO { get; set; }
         [StringLength(70)]
+        [Display(Name = "Descrição")]
         public string DESCRICAO { get; set; }
 
         [StringLength(70)]
+        [Display(Name = "DLL")]
         public string DLL { get; set; }
 
         [StringLength(70)]
+        [Display(Name = "Menu")]
         public string MENU { get; set; }
 
         [StringLength(1)]
+        [Display(Name = "Multi Empresa")]
         public string EMP { get; set; }
 
         [NotMapped]
@@ -34,8 +38,17 @@
 
         [StringLength(1)]
         [Required]
+        [Display(Name = "Grava Log")]
         public string GRAVA_LOG { get; set; }
-        
+
+        [StringLength(1)]
+        [Display(Name = "Programa Auxiliar")]
+        public string PROGRAMA_AUXILIAR { get; set; }
+
+        [StringLength(100)]
+        [Display(Name = "Caminho do Banco")]
+        public string CAMINHO_BANCO { get; set; }
+
         [Required]
         public int ORDEM { get; set; }
     }
